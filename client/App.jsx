05502e8bb0c32c36fcf9c83e73a1c05c7a47bbae
Ryan Huie <rhpurple1023@gmail.com>
2022-08-08 17:15:39 -0700

    In progress FE setup

--- client/App.jsx
+++ client/App.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import {
+    Routes,
+    Route,
+    Link
+} from 'react-router-dom'
 
 export default function App() {
-    function getReq(){
-        fetch ('http://localhost:3000/test')
-         .then (res=> res.json())
-         .then (res => console.log(res))
-    }
+    // function getReq(){
+    //     fetch ('http://localhost:3000/test')
+    //      .then (res=> res.json())
+    //      .then (res => console.log(res))
+    // }
     return(
         <div>
-            <h1>It Worked!</h1>
-            <button onClick={getReq}>ClickMe</button>
+            <Routes>
+                <Route path='/login' element={<Login props={props}/>}></Route>
+            </Routes>
         </div>
     )
 }
