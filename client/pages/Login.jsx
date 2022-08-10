6c6d9caa1ffbb542f16b30e3cca4df899ec38ce8
Ryan Huie <rhpurple1023@gmail.com>
2022-08-09 19:42:36 -0700

    FE benchmark

--- client/pages/Login.jsx
+++ client/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Link } from "react-router-dom";
-import SignUp from './SignUp';
+import SignUp from './SignUp.jsx';
 
 const Login = (props) => {
 
