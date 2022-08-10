a2e1423726003ab2659d515d61accb6e4062deae
Elliott <99830587+elliottburr@users.noreply.github.com>
2022-08-10 16:46:59 -0700

    Revert "Mark/auth"

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -33,49 +33,38 @@ authController.checkJWT = async (req, res, next) => {
 }
 
 authController.signup = async (req, res, next) => {
-    console.log('made it to signup controller');
-    const { username, password } = req.body;
-    if(!username || !password){
-        console.log('you must provide a username and password');
-        return next({message: {error: 'error'}});
-    };
-    const user = await db.query("SELECT * FROM users WHERE username = $1", [username]);
-    console.log(user.rows);
-    if(user.rows <= 0){
-        console.log('this is a new one');
-        bcrypt.hash(password, saltRounds, (err, hash) => {
-            if(err){
-                console.log('there was an error with the hashing');
-                return next(err);
-            }
-            db.query('INSERT INTO users (username, password) VALUES ($1, $2)', [username, hash]);
-            res.locals.returnSignup = 'signup-successful';
-            return next();
-        });
-    } else {
-        console.log('This username already exists');
-        return next('error');
-    }
+    const { username, password } = await req.body;
+    if(!username || !password) res.status(422).send({error: 'You must provide an email and a password.'});
+
+    bcrypt.hash(password, saltRounds, (err, hash) => {
+        if(err) return next({message: { err: 'There was an error with the signup request'}});
+        db.query('INSERT INTO users (username, password) VALUES ($1, $2)', [username, hash])
+            .then(() => {
+                res.locals.tokenUsername = username;
+                res.locals.tokenHash = hash;
+            })
+            .then(() => {return next()})
+            .catch((err) => {return next(err);})
+    });
 }
 
 authController.login = async (req, res, next) => {
-    const { username, inputPassword } = req.body;
+    const { username, password } = await req.body;
     let data = await db.query('SELECT * FROM users WHERE username = $1', [username]);
-    console.log(data);
-    let dataHash = data.rows[0];
-    const { password } = dataHash;
-    if(data.rows <= 0){
-    }
-    bcrypt.compare(inputPassword, password, (err, response) => {
+    // await console.log(data);
+    let userInfo = await data.rows[0];
+    await bcrypt.compare(password, userInfo.password, (err, response) => {
         if(err) {
             return next(err);
         }
+
+        res.locals.tokenUsername = username;
+        res.locals.hash = userInfo.password;
+
         if(response === false){
             res.locals.elevenIfTrue = 0;
-            console.log('incorrect');
         } else {
             res.locals.elevenIfTrue = 11;
-            console.log('correct');
         }
         return next();
     });
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-const app = express();
+const app = express()
 const PORT = 3000
 
 app.use(express.json()) // built in middleware parsing incoming JSON requests and puts parsed data in req.body
@@ -10,7 +10,7 @@ app.use(cors());
 const getQuestion = require ('./controllers/getQuestion');
 const authR = require('./routers/authenticationRouter');
 
-app.use(express.static(path.resolve(__dirname, '../dist/')));
+app.use(express.static(path.resolve(__dirname, "../dist/bundle.js")))
 
 
 app.get('/getQuestion', getQuestion.nextQ, (req, res) => {
